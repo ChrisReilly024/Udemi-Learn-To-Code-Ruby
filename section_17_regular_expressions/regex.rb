7b4cde7670b56c16a4c4257eba65f556de5fcf39
ChrisReilly024 <creilly024@gmail.com>
2020-05-08 21:01:50 -0400

    notes update

---
+++ section_17_regular_expressions/regex.rb
@@ -0,0 +1,17 @@
+# phrase = 'The Ruby Programming Language is Amazing'
+
+# puts phrase.start_with?("The Ruby")
+
+# def custom_start_with?(string,substring)
+#     # string.start_with?(substring)
+#     string[0...substring.length] == substring
+# end
+
+# def custom_end_with?(string,substring)
+#     # string.end_with?(substring)
+#     string[-(substring.length)..-1] == substring
+# end
+
+# p custom_start_with?('hello', 'he')
+# p custom_end_with?('bootcamp', 'amp')
+
